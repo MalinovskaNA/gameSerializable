45f1b4c384c1208e2ab7120fc62d27de7257dcf1
MalinovskaNA <delenmir@yandex.ru>
2022-04-07 10:52:22 +0300

    refactoring code

--- src/ru/netology/lessonN/Main.java
+++ src/ru/netology/lessonN/Main.java
@@ -11,34 +11,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        GameProgress gp1 = new GameProgress(10, 5, 4, 22);
-        GameProgress gp2 = new GameProgress(7, 7, 5, 54);
-        GameProgress gp3 = new GameProgress(6, 3, 1, 13);
+        GameProgress[] gps = {new GameProgress(10, 5, 4, 22),
+                new GameProgress(7, 7, 5, 54),
+                new GameProgress(6, 3, 1, 13)};
+
 
         File dirSavegames = new File("D://Games//savegames");
         File savezipFile = new File("D://Games//savegames//savezip.zip");
 
-        File save1 = new File(dirSavegames, "save1.dat");
-        File save2 = new File(dirSavegames, "save2.dat");
-        File save3 = new File(dirSavegames, "save3.dat");
+        File[] saveFiles = {new File(dirSavegames, "save1.dat"),
+                new File(dirSavegames, "save2.dat"),
+                new File(dirSavegames, "save3.dat")};
 
-        saveGame(String.valueOf(save1), gp1);
-        saveGame(String.valueOf(save2), gp2);
-        saveGame(String.valueOf(save3), gp3);
+        for (int i = 0; i < 3; i++) {
+            saveGame(saveFiles[i], gps[i]);
+        }
 
-        zipFiles(String.valueOf(savezipFile), save1, save2, save3);
+        zipFiles(String.valueOf(savezipFile), saveFiles);
 
-        if (save1.delete())
-            System.out.println("Файл " + save1 + " удален");
-        if (save2.delete())
-            System.out.println("Файл " + save2 + " удален");
-        if (save3.delete())
-            System.out.println("Файл " + save3 + " удален");
-
+        for (File file : saveFiles) {
+            if (file.delete())
+                System.out.println("Файл " + file.getName() + " из каталога " + file.getParent() + " удален");
+        }
     }
 
-    public static void saveGame(String path, GameProgress gp) {
-        try (FileOutputStream fos = new FileOutputStream(path);
+    public static void saveGame(File path, GameProgress gp) {
+        try (FileOutputStream fos = new FileOutputStream(String.valueOf(path));
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(gp);
         } catch (Exception ex) {
